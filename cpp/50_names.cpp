d905ed9f4ad18174acdd9bb3f01767e30161c62e
Wisdom Matthew <talk2wisdommatt@gmail.com>
2021-01-27 23:23:02 +0100

    Added 50_names.cpp assignments comments.

---
+++ cpp/50_names.cpp
@@ -0,0 +1,12 @@
+// C++ assingment 
+// 
+// Instruction: Print your name 50 times using do and do-while loop.
+// Lecturer: Emma
+// Course: CMS 114
+// Date: Jan-26-2021
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
