d5581d234c32a3dd824740bf995a0bbeaf9817f1
Wisdom Matthew <talk2wisdommatt@gmail.com>
2021-01-27 23:29:28 +0100

    Added while loop functionality 50_names.cpp.

--- cpp/50_names.cpp
+++ cpp/50_names.cpp
@@ -1,6 +1,6 @@
 // C++ assingment 
 // 
-// Instruction: Print your name 50 times using do and do-while loop.
+// Instruction: Print your name 50 times using while and do-while loop.
 // Lecturer: Emma
 // Course: CMS 114
 // Date: Jan-26-2021
@@ -8,5 +8,19 @@
 using namespace std;
 
 int main() {
+    // Printing my name 50 times using while loop.
+    string myName = "Wisdom Matthew";
+    int count = 1;
+    int loop = 50;
+    cout << "Using while loop :" << endl << "--------------------------------------------" << endl << endl;
+    while (count <= loop)
+    {
+        cout << myName << endl;
+        count++;
+    }
+
+    // Printing my name 50 times using do-while loop.
+    // setting count to 0 again;
+
     return 0;
 }
